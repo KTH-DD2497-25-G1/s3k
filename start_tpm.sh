7760b972a4c9dc27ee28f72f75bfaf2fc536c831
DeenkaS <deenka@gmail.com>
2025-12-08 17:02:56 +0100

    Some bad practices with huge commits. Have forgotten to working on tpm, still untested, will need some assisstance to test

--- start_tpm.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-mkdir tpm0
-swtpm socket \
-  --tpm2 \
-  --tpmstate dir=/home/deenka/repos/system-sec-course/rs-s3k/tpm0 \
-  --ctrl type=unixio,path=/home/deenka/repos/system-sec-course/rs-s3k/tpm0/swtpm-sock \
-  --log file=tpm0/swtpm.log,level=20
